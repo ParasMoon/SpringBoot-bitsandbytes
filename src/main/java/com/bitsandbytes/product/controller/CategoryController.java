7c3cb1974f7748542f40727bb36389f641da3a92
Paras Moon <parasmoon21@gmail.com>
2025-06-08 17:38:51 +0530

    Updated controllers and SecurityConfig

--- src/main/java/com/bitsandbytes/product/controller/CategoryController.java
+++ src/main/java/com/bitsandbytes/product/controller/CategoryController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -42,6 +43,7 @@ public class CategoryController {
             responseCode = "201",
             description = "CREATED"
     )
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO) {
         return new ResponseEntity<>(categoryService.createCategory(categoryDTO), HttpStatus.CREATED);
@@ -60,6 +62,7 @@ public class CategoryController {
              summary = "Delete category by categoryId",
              description = "REST API to delete category by categoryId"
      )
+     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
       @DeleteMapping("/{id}")
       public String deleteCategory(@PathVariable Long id) {
        return categoryService.deleteCategory(id);
